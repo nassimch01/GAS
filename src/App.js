efeef87f2e9a74d8bbd3031e2480a07837f4a604
nassimch01 <chakrounnassim001@gmail.com>
2025-08-26 12:39:00 +0100

    erreur

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './assets/css/templatemo-style.css';
+import './public/assets/css/templatemo-style.css';
 import Home from './components/Home';
-import Contact from './components/Contact';
-import AboutUs from './components/AboutUs';
-import Photos from './components/Photos';
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <Contact />
-      <AboutUs />
-      <Photos />
 
     </div>
   );
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -5,49 +5,10 @@ function Photos() {
         <div>
 
 
-            <div id="loader-wrapper">
-                <div id="loader"></div>
 
-                <div className="loader-section section-left"></div>
-                <div className="loader-section section-right"></div>
 
-            </div>
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="index.html">
-                        <i className="fas fa-film mr-2"></i>
-                        Catalog-Z
-                    </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <i className="fas fa-bars"></i>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link nav-link-1 active" aria-current="page" href="index.html">Photos</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link nav-link-2" href="videos.html">Videos</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link nav-link-3" href="about.html">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link nav-link-4" href="contact.html">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
 
-            <div className="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="img/hero.jpg">
-                <form className="d-flex tm-search-form">
-                    <input className="form-control tm-search-input" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success tm-search-btn" type="submit">
-                        <i className="fas fa-search"></i>
-                    </button>
-                </form>
-            </div>
+
 
             <div className="container-fluid tm-container-content tm-mt-60">
                 <div className="row mb-4">
@@ -203,43 +164,7 @@ function Photos() {
                 </div>
             </div>
 
-            <footer className="tm-bg-gray pt-5 pb-3 tm-text-gray tm-footer">
-                <div className="container-fluid tm-container-small">
-                    <div className="row">
-                        <div className="col-lg-6 col-md-12 col-12 px-5 mb-5">
-                            <h3 className="tm-text-primary mb-4 tm-footer-title">About Catalog-Z</h3>
-                            <p>Integer ipsum odio, pharetra ac massa ac, pretium facilisis nibh. Donec lobortis consectetur molestie. Nullam nec diam dolor. Fusce quis viverra nunc, sit amet varius sapien.</p>
-                        </div>
-                        <div className="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
-                            <h3 className="tm-text-primary mb-4 tm-footer-title">Our Links</h3>
-                            <ul className="tm-footer-links pl-0">
-                                <li><a href="#">Advertise</a></li>
-                                <li><a href="#">Support</a></li>
-                                <li><a href="#">Our Company</a></li>
-                                <li><a href="#">Contact</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
-                            <ul className="tm-social-links d-flex justify-content-end pl-0 mb-5">
-                                <li className="mb-2"><a href="https://facebook.com"><i className="fab fa-facebook"></i></a></li>
-                                <li className="mb-2"><a href="https://twitter.com"><i className="fab fa-twitter"></i></a></li>
-                                <li className="mb-2"><a href="https://instagram.com"><i className="fab fa-instagram"></i></a></li>
-                                <li className="mb-2"><a href="https://pinterest.com"><i className="fab fa-pinterest"></i></a></li>
-                            </ul>
-                            <a href="#" className="tm-text-gray text-right d-block mb-2">Terms of Use</a>
-                            <a href="#" className="tm-text-gray text-right d-block">Privacy Policy</a>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-8 col-md-7 col-12 px-5 mb-3">
-                            Copyright 2020 Catalog-Z Company. All rights reserved.
-                        </div>
-                        <div className="col-lg-4 col-md-5 col-12 px-5 text-right">
-                            Designed by <a href="https://templatemo.com" className="tm-text-gray" rel="sponsored" target="_parent">TemplateMo</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+
             {/*    <script src="js/plugins.js"></script>
     <script>
         $(window).on("load", function() {
